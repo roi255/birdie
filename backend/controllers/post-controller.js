4f217368587259fe85b3a06e7e3c7a5dfb5c12ae
amani <amani@rayomi.co.tz>
2024-06-14 14:59:29 +0300

    implements posts methods

--- backend/controllers/post-controller.js
+++ backend/controllers/post-controller.js
@@ -1,3 +1,4 @@
+import Notification from "../models/notification-model.js";
 import Post from "../models/post-model.js";
 import User from "../models/user-model.js";
 import { v2 as cloudinary } from "cloudinary";
@@ -38,12 +39,23 @@ export const createPost = async (req, res) => {
 //* function to get all posts
 export const getPosts = async (req, res) => {
   try {
-    const posts = await Post.find();
-    res.status(200).json(posts);
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: ["username", "fullName", "profilePic"],
+      });
+    if (posts.length === 0) {
+      return res.status(200).json([]);
+    }
+    res.status(200).json([posts]);
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
+    console.log("Error getting posts", error);
+    res.status(500).json({ message: error.message });
   }
 };
 
@@ -89,6 +101,119 @@ export const commentPost = async (req, res) => {
     res.status(200).json(post);
   } catch (error) {
     console.log("Error commenting on post: ", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//* like and unlike post
+
+export const likeUnlikePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const userId = req.user._id;
+
+    const post = await Post.findById(postId);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    if (post.likes.includes(userId)) {
+      await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+      await Post.updateOne({ _id: postId }, { $pull: { likedPosts: postId } });
+    } else {
+      post.likes.push(userId);
+      await User.updateOne({ _id: userId }, { $push: { likedPosts: postId } });
+      await post.save();
+
+      //send notification to the author of the post
+      const newNotification = new Notification({
+        type: "like",
+        from: userId,
+        to: post.user,
+      });
+      await newNotification.save();
+      res.status(200).json({ message: "You liked this post" });
+    }
+  } catch (error) {
+    console.log("Error liking post: ", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//* get all the liked posts
+
+export const getLikedPosts = async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    const likedPosts = await Post.find({ _id: { $in: user.likedPosts } })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: ["username", "fullName", "profilePic"],
+      })
+      .populate({
+        path: "likes.user",
+        select: ["username", "fullName", "profilePic"],
+      });
+    res.status(200).json(likedPosts);
+  } catch (error) {
+    console.log("Error getting liked posts: ", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//* get posts of people you are following
+
+export const getFollowingPosts = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const following = user.following;
+    const feed = await Post.find({ user: { $in: following } })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: ["username", "fullName", "profilePic"],
+      });
+
+    res.status(200).json(feed);
+  } catch (error) {
+    console.log("Error getting feed: ", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//* get user posts
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const posts = await Post.find({ user: user._id })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: ["username", "fullName", "profilePic"],
+      });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log("Error getting user posts: ", error);
     res.status(500).json({ message: error.message });
   }
 };
--- backend/routes/post-routes.js
+++ backend/routes/post-routes.js
@@ -1,13 +1,25 @@
 import express from "express";
+import {
+  commentPost,
+  createPost,
+  deletePost,
+  getFollowingPosts,
+  getLikedPosts,
+  getPosts,
+  getUserPosts,
+  likeUnlikePost,
+} from "../controllers/post-controller.js";
 import { protectedRoute } from "../middleware/protected-route.js";
-import { commentPost, createPost, deletePost, getPosts } from "../controllers/post-controller.js";
 
 const router = express.Router();
 
 router.post("/create", protectedRoute, createPost);
-// router.post("like/:id", likeUnlikePost);
+router.post("/like/:id", protectedRoute, likeUnlikePost);
 router.post("/comment/:id", protectedRoute, commentPost);
 router.delete("/:id", protectedRoute, deletePost);
-// router.get("/posts", protectedRoute, getPosts);
+router.get("/all", protectedRoute, getPosts);
+router.get("/following", protectedRoute, getFollowingPosts);
+router.get("/liked/:id", protectedRoute, getLikedPosts);
+router.get("/user/:username", protectedRoute, getUserPosts);
 
 export default router;
