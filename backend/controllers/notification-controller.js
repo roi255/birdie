dc1900ec4eb777c16a79a39814ff0f434efda052
amani <amani@rayomi.co.tz>
2024-06-28 16:01:19 +0300

    backend server

--- backend/controllers/notification-controller.js
+++ backend/controllers/notification-controller.js
@@ -21,8 +21,34 @@ export const deleteNotifications = async (req, res) => {
   try {
     const userId = req.user._id;
 
-    res.status(200).json({ message: "Notification deleted" });
+    await Notification.deleteMany({ to: userId });
+
+    res.status(200).json({ message: "Notifications deleted" });
   } catch (error) {
+    console.log("Error deleting notifications", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//* delete single notification
+
+export const deleteNotification = async (req, res) => {
+  try {
+    const notificationId = req.params.id;
+    const userId = req.user._id;
+
+    const notification = await Notification.findById(notificationId);
+
+    if (!notification) return res.status(404).json({ message: "Notification not found" });
+
+    if (notification.to.toString() !== userId.toString()) {
+      return res.status(403).json({ message: "You are not allowed to delete this notification" });
+    }
+
+    await Notification.findByIdAndDelete(notificationId);
+    res.status(200).json({ message: "Notification deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting notification", error.message);
     res.status(500).json({ message: error.message });
   }
 };
--- backend/routes/notification-routes.js
+++ backend/routes/notification-routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectedRoute } from "../middleware/protected-route.js";
-import { deleteNotifications, getNotifications } from "../controllers/notification-controller.js";
+import { deleteNotification, deleteNotifications, getNotifications } from "../controllers/notification-controller.js";
 
 const router = express.Router();
 
 router.get("/", protectedRoute, getNotifications);
 router.delete("/", protectedRoute, deleteNotifications);
+router.delete("/:id", protectedRoute, deleteNotification);
 
 export default router;
